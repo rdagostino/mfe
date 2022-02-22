57f129f8b41a5878fa855d9b48b40594664e2f83
Ryan D'Agostino <ryan.dagostino@ey.com>
2022-02-22 14:24:44 -0500

    Added mui class prefix

--- container/src/App.js
+++ container/src/App.js
@@ -1,14 +1,20 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
-// App
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
