52a9100abd53afe6a7a70a639097a9d0d1dc1c68
Ryan D'Agostino <ryan.dagostino@ey.com>
2022-02-22 14:03:16 -0500

    Added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 // App
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
